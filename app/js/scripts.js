7e2e267af687621e09ddc0845ba314dbdc5657b6
Unknown <typict@ukr.net>
2017-12-05 18:59:49 +0200

    clean_js

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,123 +1,95 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
 
-  /* 1. Visualizing things on Hover - See next part for action on click */
-  $('#stars li').on('mouseover', function(){
-    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+    $('#stars li').on('mouseover', function() {
+        var onStar = parseInt($(this).data('value'), 10);
 
-    // Now highlight all the stars that's not after the current hovered star
-    $(this).parent().children('li.star').each(function(e){
-      if (e < onStar) {
-        $(this).addClass('hover');
-      }
-      else {
-        $(this).removeClass('hover');
-      }
+        $(this).parent().children('li.star').each(function(e) {
+            if (e < onStar) {
+                $(this).addClass('hover');
+            } else {
+                $(this).removeClass('hover');
+            }
+        });
+
+    }).on('mouseout', function() {
+        $(this).parent().children('li.star').each(function(e) {
+            $(this).removeClass('hover');
+        });
     });
 
-  }).on('mouseout', function(){
-    $(this).parent().children('li.star').each(function(e){
-      $(this).removeClass('hover');
+
+    $('#stars li').on('click', function() {
+        var onStar = parseInt($(this).data('value'), 10);
+        var stars = $(this).parent().children('li.star');
+
+        for (i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('selected');
+        }
+
+        for (i = 0; i < onStar; i++) {
+            $(stars[i]).addClass('selected');
+        }
     });
-  });
-
-
-  /* 2. Action to perform on click */
-  $('#stars li').on('click', function(){
-    var onStar = parseInt($(this).data('value'), 10); // The star currently selected
-    var stars = $(this).parent().children('li.star');
-
-    for (i = 0; i < stars.length; i++) {
-      $(stars[i]).removeClass('selected');
-    }
-
-    for (i = 0; i < onStar; i++) {
-      $(stars[i]).addClass('selected');
-    }
-
-
-
-  });
-
-
 });
 
 
-
-
-  $(function(){
-    $('.share').click(function(){
+$(function() {
+    $('.share').click(function() {
         $('.share_with').toggleClass('open')
-                    });
+    });
+});
+
+jQuery(function($) {
+    $(document).mouseup(function(e) {
+        var div = $(".share_with");
+        if (!div.is(e.target)
+            &&
+            div.has(e.target).length === 0) {
+            $('.share_with').removeClass('open')
+        }
+    });
 });
 
 
-
-jQuery(function($){
-  $(document).mouseup(function (e){ // событие клика по веб-документу
-    var div = $(".share_with"); // тут указываем ID элемента
-    if (!div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0) { // и не по его дочерним элементам
-      $('.share_with').removeClass('open')
-    }
-  });
-});
-
-
-  $(function(){
-    $('.close').click(function(){
+$(function() {
+    $('.close').click(function() {
         $('.share_with').removeClass('open')
-                    });
+    });
 });
-
-
-
 
 
 if (window.innerWidth < 600) {
 
-var controls = document.querySelectorAll('.controls');
-for(var i=0; i<controls.length; i++){
-    controls[i].style.display = 'inline-block';
-}
+    var controls = document.querySelectorAll('.controls');
+    for (var i = 0; i < controls.length; i++) {
+        controls[i].style.display = 'inline-block';
+    }
+    var slides = document.querySelectorAll('#slides .item');
+    var currentSlide = 0;
+    function nextSlide() {
+        goToSlide(currentSlide + 1);
+    }
+    function previousSlide() {
+        goToSlide(currentSlide - 1);
+    }
+    function goToSlide(n) {
+        slides[currentSlide].className = 'item';
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].className = 'item showing';
+    }
+    var playing = true;
 
-var slides = document.querySelectorAll('#slides .item');
-var currentSlide = 0;
+    var next = document.getElementById('next');
+    var previous = document.getElementById('previous');
 
+    next.onclick = function() {
 
-function nextSlide(){
-    goToSlide(currentSlide+1);
-}
+        nextSlide();
+    };
+    previous.onclick = function() {
 
-function previousSlide(){
-    goToSlide(currentSlide-1);
-}
-
-function goToSlide(n){
-    slides[currentSlide].className = 'item';
-    currentSlide = (n+slides.length)%slides.length;
-    slides[currentSlide].className = 'item showing';
-}
-
-
-var playing = true;
-
-
-
-
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
-
-next.onclick = function(){
-
-    nextSlide();
-};
-previous.onclick = function(){
-
-    previousSlide();
-};
+        previousSlide();
+    };
 
 }
-
-
-
