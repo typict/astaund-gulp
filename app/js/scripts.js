9cced5bf2bb208f96f6caf562bc272cee57f0044
Unknown <typict@ukr.net>
2017-12-12 12:26:25 +0200

    js_600

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -58,38 +58,45 @@ $(function() {
     });
 });
 
+function windowSize(){
+    if($(window).width()<='600'){
 
-if (window.innerWidth < 600) {
+        var controls = document.querySelectorAll('.controls');
+        for (var i = 0; i < controls.length; i++) {
+            controls[i].style.display = 'inline-block';
+        }
+        var slides = document.querySelectorAll('#slides .item');
+        var currentSlide = 0;
+        function nextSlide() {
+            goToSlide(currentSlide + 1);
+        }
+        function previousSlide() {
+            goToSlide(currentSlide - 1);
+        }
+        function goToSlide(n) {
+            slides[currentSlide].className = 'item';
+            currentSlide = (n + slides.length) % slides.length;
+            slides[currentSlide].className = 'item showing';
+        }
+        var playing = true;
 
-    var controls = document.querySelectorAll('.controls');
-    for (var i = 0; i < controls.length; i++) {
-        controls[i].style.display = 'inline-block';
+        var next = document.getElementById('next');
+        var previous = document.getElementById('previous');
+
+        next.onclick = function() {
+
+            nextSlide();
+        };
+        previous.onclick = function() {
+
+            previousSlide();
+        };
+
+
+    }else{
+        // экран больше 600 px в ширниу
     }
-    var slides = document.querySelectorAll('#slides .item');
-    var currentSlide = 0;
-    function nextSlide() {
-        goToSlide(currentSlide + 1);
-    }
-    function previousSlide() {
-        goToSlide(currentSlide - 1);
-    }
-    function goToSlide(n) {
-        slides[currentSlide].className = 'item';
-        currentSlide = (n + slides.length) % slides.length;
-        slides[currentSlide].className = 'item showing';
-    }
-    var playing = true;
-
-    var next = document.getElementById('next');
-    var previous = document.getElementById('previous');
-
-    next.onclick = function() {
-
-        nextSlide();
-    };
-    previous.onclick = function() {
-
-        previousSlide();
-    };
-
 }
+$(window).load(windowSize);
+$(window).resize(windowSize);
+$(window).on('load resize',windowSize);
